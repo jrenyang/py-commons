943b6831c06d37db28aed4919f24e7edb1cd1702
Art Yang <101048971+jrenyang@users.noreply.github.com>
2022-04-26 15:29:10 +0800

    update StringUtils

--- py_commons/utils/string_utils.py
+++ py_commons/utils/string_utils.py
@@ -24,6 +24,32 @@ class StringUtils:
         
         return value.replace(source_str, target_str)
     
+    # 轉駝峰命名
+    @staticmethod
+    def to_camel_case(value):
+        result = ""
+        str = value.split("_")
+
+        for v in str:
+            result += v.capitalize()
+
+        return result
+
+    # 欄位轉駝峰命名
+    @staticmethod
+    def field_to_camel_case(value):
+        result = None
+        str = value.split("_")
+
+        if str:
+            result = str[0].lower()
+
+        for v in str[1:]:
+            result += v.capitalize()
+
+        return result
+    
+    # 取得字串長度
     @staticmethod
     def length(value:str):
         if value == None:
